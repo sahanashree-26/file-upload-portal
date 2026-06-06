fc63651283bc7ffedf5ec9406d88b1c7decd9c62
sahanashree-26 <sahanashreee26@gmail.com>
2026-06-06 21:32:05 +0530

    Initial full project submission

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const pool = require("./db");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+const fileRoutes = require("./routes/fileRoutes");
+const folderRoutes = require("./routes/folderRoutes");
+const protect = require("./middleware/authMiddleware");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/files", fileRoutes);
+app.use("/api/folders", folderRoutes);
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      message: "Database Connected Successfully",
+      time: result.rows[0],
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: "Protected route accessed successfully",
+    user: req.user,
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
